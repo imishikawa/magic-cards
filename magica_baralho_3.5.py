3104ed438c3bf6ee01564555169bd2c328f1f7ab
imishikawa <36236325+imishikawa@users.noreply.github.com>
2024-06-18 16:52:12 -0300

    Update

--- magica_baralho_3.5.py
+++ magica_baralho_3.5.py
@@ -34,7 +34,6 @@ soma = 1
 while soma==1:
        
        
-
        #print (baralho)
 
        certo=0
@@ -42,39 +41,40 @@ while soma==1:
        repetir=1
        while repetir<=Nmax:
                      random.shuffle (baralho)
-                     posicao = 0
-                     #posicaoUsuario = 5
-                     #posicao = random.randint(0,9) #posicao inicial escolhida pelo magico
-                     posicaoUsuario =  random.randint(0,9) #posição inicial escolhida por alguem da plateia
-                     #print(posicao)
-                     #print(posicaoUsuario)
+                     posicao_magico = 0
+                     #posicao_usuario = 5
+                     #posicao_magico = random.randint(0,9) #posicao inicial escolhida pelo magico
+                     posicao_usuario =  random.randint(0,9) #posição inicial escolhida por alguem da plateia
+                     #print('Posição Mágico:' posicao_magico)
+                     #print('Posição Pessoa da Platéia:' posicao_usuario)
+                     
 #Atribuição de valores:
-                     while posicao<52:
-                            numero = baralho[posicao][2]
-                            posicao = posicao+numero
-                     posicao = posicao-numero  #voltando para a carta anterior       
+                     while posicao_magico<52:
+                            numero = baralho[posicao_magico][2]
+                            posicao_magico = posicao_magico+numero
+                     posicao_magico = posicao_magico-numero  #voltando para a carta anterior       
 
-                     while posicaoUsuario<52:
-                            numero = baralho[posicaoUsuario][2]
-                            posicaoUsuario = posicaoUsuario+numero
-                     posicaoUsuario = posicaoUsuario-numero  #voltando para a carta anterior
-                     #print('Posicao Usuario',posicaoUsuario)
-                     #print('Posicao',posicao)
+                     while posicao_usuario<52:
+                            numero = baralho[posicao_usuario][2]
+                            posicao_usuario = posicao_usuario+numero
+                     posicao_usuario = posicao_usuario-numero  #voltando para a carta anterior
+                     #print('Posição Mágico:' posicao_magico)
+                     #print('Posição Pessoa da Platéia:' posicao_usuario)
                   
-                     if posicao != posicaoUsuario:
+                     if posicao_magico != posicao_usuario:
                             errado=errado+1
                                   
-                     elif posicao == posicaoUsuario:
+                     elif posicao_magico == posicao_usuario:
                             certo=certo+1
                                                             
                      repetir=repetir+1
-       print (certo)
-       print (errado)
+       print ('Total de Acertos:', certo)
+       print ('Total de Erros:', errado)
        total=certo+errado
        divisão = certo/total
-       print ("Porcentagem de acerto:", divisão*100)
+       print ('Porcentagem de acerto:', divisão*100)
 
-       x=int(input("1 para continuar, 2 para sair:"))
+       x=int(input('1 para continuar, 2 para sair:'))
        if x != 1:
               break
        else:
